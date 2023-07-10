51ae58e341506cbc1bb49384109875cefd649d4f
Rachel Okorie <jayrachel96@gmail.com>
2023-07-10 20:09:21 +0100

    add authentication

--- TaskMgt/Program.cs
+++ TaskMgt/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TaskMgt.Context;
 using TaskMgt.Extensions;
@@ -28,34 +29,43 @@ namespace TaskMgt
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            });/*
-                 .AddJwtBearer(jwt =>
-                 {
-
-                     string jwtConfig = builder.Configuration.GetSection("JwtConfig:Key").Value;
-                     string issuer = builder.Configuration.GetSection("JwtConfig:Issuer").Value;
-                     string audience = builder.Configuration.GetSection("JwtConfig:Audience").Value;
-                     var key = Encoding.ASCII.GetBytes(jwtConfig);
-
-                     jwt.SaveToken = true;
-                     jwt.TokenValidationParameters = new TokenValidationParameters
-                     {
-                         ValidateIssuerSigningKey = true,
-                         IssuerSigningKey = new SymmetricSecurityKey(key),
-                         ValidateIssuer = true,
-                         ValidateAudience = true,
-                         ValidateLifetime = true,
-                         RequireExpirationTime = true,
-                         ValidIssuer = issuer,
-                         ValidAudience = audience,
-                         *//*ClockSkew = TimeSpan.Zero*//*
-                     };
-                 });
+            });
 */
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // c.EnableAnnotations();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RMTSAPI", Version = "v1" });
+
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                            Array.Empty<string>()
+                    },
+                });
+            });
+
 
             var app = builder.Build();
 
